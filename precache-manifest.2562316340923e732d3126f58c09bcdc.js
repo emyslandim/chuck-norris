23d5be1bbf459d38d78419afc30a8a02a0e560cf
emysmoura <emydl.moura@gmail.com>
2020-06-08 17:00:43 -0300

    Updates

---
+++ precache-manifest.2562316340923e732d3126f58c09bcdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6bc95e46ac0318c37e2a50cbfb3acb",
+    "url": "/chuck-norris/index.html"
+  },
+  {
+    "revision": "3b7c9c2c4a0d58efa7bd",
+    "url": "/chuck-norris/static/css/main.f673671f.chunk.css"
+  },
+  {
+    "revision": "89e5b152a6f0f44ce92b",
+    "url": "/chuck-norris/static/js/2.1585cc12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chuck-norris/static/js/2.1585cc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7c9c2c4a0d58efa7bd",
+    "url": "/chuck-norris/static/js/main.e6adcb3d.chunk.js"
+  },
+  {
+    "revision": "cb5e238b19213ad9ba74",
+    "url": "/chuck-norris/static/js/runtime-main.a70f83fb.js"
+  },
+  {
+    "revision": "a3205fec4be1c22b4f3e7992893c4961",
+    "url": "/chuck-norris/static/media/chuck.a3205fec.png"
+  }
+]);
